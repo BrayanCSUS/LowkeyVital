7508cb9366f9c44cce969892d6291dc7425de123
William Miras <william.miras@gmail.com>
2025-02-26 23:47:50 -0800

    Added my name! - Will

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		System.out.println("214579912 - This line was added by Brayan Mejia Nunez.");
 		System.out.println("303426344 - This line was added by Matthew Anselmi.");
 		System.out.println("301746705 - This line was added by Nicole Espinoza.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("303903600 - This line was added by William Miras.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
