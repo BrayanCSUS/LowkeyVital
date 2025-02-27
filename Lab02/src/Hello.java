84874bdff4a4ae43dba4a0aeb0f761c66ae2e790
MatthewA-07 <manselmi2@csus.edu>
2025-02-26 17:01:34 -0800

    Merge branch 'main' of https://github.com/BrayanCSUS/LowkeyVital into main

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("214579912 - This line was added by Brayan Mejia Nunez.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("303426344 - This line was added by Matthew Anselmi.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
