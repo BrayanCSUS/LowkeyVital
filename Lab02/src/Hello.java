d4538715041356edc1385ecab36c2b707048644f
TheOniiSean <subombay@csus.edu>
2025-02-27 00:06:29 -0800

    I have added my name lmao xd pog champ uwu amongsus

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
 		System.out.println("303426344 - This line was added by Matthew Anselmi.");
 		System.out.println("301746705 - This line was added by Nicole Espinoza.");
 		System.out.println("303903600 - This line was added by William Miras.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("304070234 - This line was added by Sean Bombay.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
