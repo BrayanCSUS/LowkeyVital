339b7a49f424f6d09823293a4d128b1ed619fe87
PalladenCS <palladenlee@csus.edu>
2025-02-27 21:32:45 -0800

    Update Hello.java

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -10,7 +10,7 @@ public class Hello {
 		System.out.println("301746705 - This line was added by Nicole Espinoza.");
 		System.out.println("303903600 - This line was added by William Miras.");
 		System.out.println("304070234 - This line was added by Sean Bombay.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("304297539 - This line was added by Palladen Lee.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
